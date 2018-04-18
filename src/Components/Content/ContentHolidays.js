f1bb1ddb9f8c7640a5ac231ed64f0d2f96045337
IgnacioYanjari <ignacio.yanjari@mail.udp.cl>
2018-04-18 19:03:28 -0300

    barra de carga

--- src/Components/Content/ContentHolidays.js
+++ src/Components/Content/ContentHolidays.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {Card, CardText} from 'material-ui/Card';
+import {CircularProgress} from 'material-ui';
 import {notification} from 'antd';
 import shortid from 'shortid';
 
@@ -7,11 +8,13 @@ class ContentHolidays extends Component{
 
   constructor(props){
     super(props);
-    this.state = { listOfHolidays : {} , today : new Date()};
+    this.state = {
+      listOfHolidays : {} ,
+      today : new Date()
+    };
   }
 
   componentDidMount(){
-
     fetch('https://www.feriadosapp.com/api/holidays.json')
       .then( (response) =>{
         return response.json()
@@ -22,75 +25,74 @@ class ContentHolidays extends Component{
       .catch( err => {
         console.log(err.name + ': ' + err.message)
       })
-}
-
-
-  FirstHoliday(){
-    let firstHoliday = false;
-    const list = this.state.listOfHolidays, today = this.state.today;
-    for( let element in list){
-      let date = list[element].date.split("-");
-      let year = today.getFullYear().toString() ,
-        month = today.getMonth()+1, day = today.getDate();
-      if( (date[1] >= month || (date[1] === month && date[2] >= day))
-          && firstHoliday === false && date[0] === year
-        ){
-          return(
-            <div>
-              {notification['success']({
-                message: 'Feriado más cercano:',
-                placement:'bottomRight',
-                duration: 20,
-                description:
-                <div>
-                  <p> Razón : {list[element].title} </p>
-                  <p> Fecha : {list[element].date} </p>
-                  <p> Extra : {list[element].extra} </p>
-                </div>
-              })}
-            </div>
-          );
-      }
     }
+
+  renderPanelHoliday(holiday){
+    return(
+      <Card key ={shortid.generate()} style={{ margin:'1% 6% 1% 6%', textAlign:'center'}}>
+        <CardText>
+          <p> {holiday.title} </p>
+          <p> {holiday.date}  </p>
+          <p> {holiday.extra} </p>
+        </CardText>
+      </Card>
+    )
   }
 
-  // <Card key ={shortid.generate()} style={{ margin:'1% 6% 1% 6%', textAlign:'center'}}>
-  // <CardTitle
-  // title={'El dia feriado más cercano es :\n'+
-  // date[0] + '-' + date[1] + '-' + date[2]}/>
-  // </Card>
-
-  renderHolidays(holiday){
-    let date = holiday.date.split("-");
-    let apiYear = date[0], today = this.state.today;
-    let year = today.getFullYear().toString();
-    const isMyYear = (apiYear.substr(0,4) === year );
-    if( isMyYear){
-      return(
-        <Card key ={shortid.generate()} style={{ margin:'1% 6% 1% 6%', textAlign:'center'}}>
-          <CardText>
-            <p> {holiday.title} </p>
-            <p> {holiday.date}  </p>
-            <p> {holiday.extra} </p>
-          </CardText>
-        </Card>
-    )}
+  renderAlert(holiday){
     return(
-        <div key ={shortid.generate()}> </div>
+      <div key ={shortid.generate()}>
+        {this.renderPanelHoliday(holiday)}
+        {notification['success']({
+          message: 'Feriado más cercano:',
+          placement:'bottomRight',
+          duration: 20,
+          description:
+          <div>
+            <p> Razón : {holiday.title} </p>
+            <p> Fecha : {holiday.date} </p>
+            <p> Extra : {holiday.extra} </p>
+          </div>
+        })}
+      </div>
     );
   }
 
+  renderHolidays(holidays){
+    let firstHoliday=true;
+    let today = this.state.today;
+    let year = today.getFullYear().toString() ,
+      month = today.getMonth()+1, day = today.getDate();
+    return Object.keys(holidays).map( key => {
+      let holiday = holidays[key];
+      let date = holiday.date.split("-");
+      let apiYear = date[0]
+      const isMyYear = (apiYear.substr(0,4) === year );
+      if( isMyYear){
+        if( (date[1] >= month || (date[1] === month && date[2] >= day) ) && firstHoliday) {
+            firstHoliday=false;
+            return(this.renderAlert(holiday))
+          }
+        return(this.renderPanelHoliday(holiday));
+      }
+      return(
+          <div key ={shortid.generate()}> </div>
+      );
+    });
+  }
+
   render(){
-    let holidays = this.state.listOfHolidays , today = this.state.today;
-    let month = today.getMonth()+1, day = today.getDate();
-    if( month.toString().length < 2 )
-      month = '0' + month.toString();
-    if( day.toString().length < 2)
-      day = '0' + day.toString();
+    let holidays = this.state.listOfHolidays;
+    console.log('largo ' + Object.keys(holidays).length)
     return(
       <div>
-        {this.FirstHoliday()}
-        {Object.keys(holidays).map(value => this.renderHolidays(holidays[value]))}
+
+        { Object.keys(holidays).length === 0 ?(
+            <CircularProgress style={{textAlign:'center'}}size={140} thickness={5} />
+          ):(
+            this.renderHolidays(holidays)
+          )
+        }
       </div>
     )
   }
